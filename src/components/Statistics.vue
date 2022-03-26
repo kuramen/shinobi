389fa712003e57538f2497c29878c49544d7ea64
Clément Brunie <brunie.clement@gmail.com>
2022-03-27 00:40:03 +0100

    fix statistics font size on mobile and paddings

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -30,35 +30,55 @@ export default {
 <style lang="scss">
 .section.statistics {
     background-color: $primary-color;
-    padding: 100px 5vw;
-    width: calc(100% - 10vw);
     max-width: 100%;
+    padding: 100px 0;
     margin: 0;
+
+    @media (max-width:1080px){
+        padding: 100px 20px;
+        width: calc(100% - 40px);
+    }
 
     h1, h3 {
         color: $background-color;
     }
 
-    .statistics-item {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-        align-items: center;
-        text-align: center;
-
-        h2 {
-            font-size: 96px;
-            font-weight: 600;
-        }
-
-        h3 {
-            font-size: 40px;
-            font-weight: 400;
-        }
+    .content, .header {
+        max-width: 1080px;
+        width: 100%;
+        margin: 0 auto;
     }
 
     .content {
         margin-top: 40px;
+
+        .statistics-item {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            align-items: center;
+            text-align: center;
+
+            h2 {
+                font-size: 96px;
+                font-weight: 600;
+            }
+
+            h3 {
+                font-size: 40px;
+                font-weight: 400;
+            }
+
+            @media (max-width:750px){
+                h2 {
+                    font-size: 60px;
+                }
+
+                h3 {
+                    font-size: 26px;
+                }
+            }
+        }
     }
     
 }
